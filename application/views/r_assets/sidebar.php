c3f217a4abc41356c586be6bade3c518e6f12a5a
tina <myeclass2021@gmail.com>
2024-10-18 11:35:43 +0800

    update sidebar pc

--- application/views/r_assets/sidebar.php
+++ application/views/r_assets/sidebar.php
@@ -80,7 +80,7 @@
                                     <nav class="sb-sidenav-menu-nested nav">
                                         <a class="nav-link" href="<?php echo site_url('VitalSign/index'); ?>">Initial Check-Up</a>
                                         <a class="nav-link" href="<?php echo site_url('medication/index'); ?>">Patient History</a>
-                                        <a class="nav-link" href="<?php echo site_url('diagnosis/index'); ?>">Diagnosis</a>
+                                        <a class="nav-link" href="<?php echo site_url('diagnosis/index'); ?>">Treatments</a>
                                         <a class="nav-link" href="<?php echo site_url('laboratorytests/index'); ?>">Laboratory Record</a>
                                         <!-- Add more sub-items here if needed -->
                                     </nav>
